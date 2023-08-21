c212296591844fa4d29768e24f1ea669bcd6d9dc
gtanchak <ghtanchak77@gmail.com>
2023-08-21 23:43:17 +0530

    resolved responsive issue

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -2,65 +2,47 @@ import { buyPupa, managePurchase } from "@/services/stripe";
 import { getThemeById, getThemes, postTheme } from "@/services/theme";
 import { toggleThemeLike, toggleThemeSave } from "@/services/toggle";
 import { getUser, toggleFollowing } from "@/services/user";
-import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export default function Dashboard() {
   const router = useRouter();
 
   return (
-    <div className="relative top-24">
-      Dashboard
-      <button className="m-6" onClick={postTheme}>
-        Post Theme
-      </button>
-      <button className="m-6" onClick={getThemes}>
-        Get All Theme
-      </button>
-      <button
-        className="m-6"
-        onClick={() => getThemeById("clli96o7z0001t7nk3eom3cjh")}
-      >
-        Get Theme By Id
-      </button>
-      <button
-        className="m-6"
-        onClick={() => getUser("clle7kyd60000t7vgwys43514")}
-      >
-        Get User
-      </button>
-      <button
-        className="m-6"
-        onClick={() => toggleThemeLike("clli96o7z0001t7nk3eom3cjh")}
-      >
-        Toggle Like
-      </button>
-      <button
-        className="m-6"
-        onClick={() => toggleThemeSave("clli96o7z0001t7nk3eom3cjh")}
-      >
-        Toggle Save
-      </button>
-      <button className="m-6" onClick={() => buyPupa()}>
-        Buy Pupa
-      </button>
-      <button
-        className="m-6"
-        onClick={() => {
-          if (router.query.session_id) {
-            console.log(router.query.session_id);
-            managePurchase(router.query.session_id as string);
-          }
-        }}
-      >
-        Manage Purchase
-      </button>
-      <button
-        className="m-6"
-        onClick={() => toggleFollowing("clli5kos500007ou0ghjzrko9")}
-      >
-        Toggle Following
-      </button>
+    <div className="backdrop-blur-2xl bg-white/40 min-h-screen">
+      <div className="relative top-24 2xl:max-w-screen-2xl max-w-screen-xl mx-auto flex">
+        <div className="basis-1/6 ">sidebar</div>
+        <div className="basis-5/6">
+          Dashboard
+          <button onClick={postTheme}>Post Theme</button>
+          <button onClick={getThemes}>Get All Theme</button>
+          <button onClick={() => getThemeById("clli96o7z0001t7nk3eom3cjh")}>
+            Get Theme By Id
+          </button>
+          <button onClick={() => getUser("clle7kyd60000t7vgwys43514")}>
+            Get User
+          </button>
+          <button onClick={() => toggleThemeLike("clli96o7z0001t7nk3eom3cjh")}>
+            Toggle Like
+          </button>
+          <button onClick={() => toggleThemeSave("clli96o7z0001t7nk3eom3cjh")}>
+            Toggle Save
+          </button>
+          <button onClick={() => buyPupa()}>Buy Pupa</button>
+          <button
+            onClick={() => {
+              if (router.query.session_id) {
+                console.log(router.query.session_id);
+                managePurchase(router.query.session_id as string);
+              }
+            }}
+          >
+            Manage Purchase
+          </button>
+          <button onClick={() => toggleFollowing("clli5kos500007ou0ghjzrko9")}>
+            Toggle Following
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="h-screen">
         <div className="w-[400px] h-[400px] absolute -left-20 -bottom-20 bg-gradient-to-tr from-yellow-200 to-orange-300 rounded-full" />
         <div className="backdrop-blur-2xl bg-white/40 h-full">
-          <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 2xl:max-w-screen-2xl max-w-screen-xl w-full gap-10">
+          <div className="flex absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 2xl:max-w-screen-2xl max-w-screen-xl w-full gap-10 items-center">
             <div className="basis-2/5">
               <p className="mb-4 text-[#23344a]">- Creative Design Template</p>
               <h1 className="text-5xl font-bold leading-[1.3] text-[#23344a]">
@@ -32,19 +32,14 @@ export default function Home() {
               </Button>
             </div>
             <div className="basis-3/5">
-              <h1>
-                {" "}
-                Meet and communicate with the best people to run projects,
-                events or other activities in a more effective and fun way.
-              </h1>
-              {/* <div className="h-[460px] w-[700px] mt-5 bg-[#ffd069] rounded-md mx-auto " />
+              <div className="2xl:h-[460px] 2xl:w-[700px] h-[400px] w-[550px] mt-5 bg-[#ffd069] rounded-md mx-auto" />
               <Image
-                className="absolute top-0 left-1/2 rounded-md shadow-md "
+                className="absolute top-0 2xl:left-1/2 left-auto right-0  rounded-md shadow-md 2xl:w-[700px] w-[600px] "
                 src="/banner.png"
                 alt="banner image"
                 width={700}
                 height={600}
-              /> */}
+              />
             </div>
           </div>
         </div>
