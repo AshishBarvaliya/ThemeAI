a303b89b8877789b93809bb472621a78579c2611
Ashish Baravaliya <vier.institute35@gmail.com>
2023-12-25 18:22:39 -0500

    fix: resolved conflicts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,11 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import NextNProgress from "nextjs-progressbar";
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+
+ReactGA.initialize(TRACKING_ID || "");
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
