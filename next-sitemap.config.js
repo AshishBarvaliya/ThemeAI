630d456e8751752f61bd807fefe5357df7eea63c
Ashish Baravaliya <vier.institute35@gmail.com>
2024-01-08 14:30:38 -0500

    feat: additional paths added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,24 @@
+const axios = require("axios");
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.themeai.io",
   generateRobotsTxt: true,
   exclude: ["/server-sitemap.xml"],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://www.themeai.io/server-sitemap.xml"],
+  additionalPaths: async () => {
+    const urls = await axios
+      .get(
+        `${process.env.NEXTAUTH_URL}/api/themes?page=1&search=&type=explore&aiOnly=false`
+      )
+      .then((res) => res.data);
+
+    const fields = urls
+      ? urls.map((theme) => ({
+          loc: `/theme/${theme.id}`,
+          lastmod: new Date().toISOString(),
+          changefreq: "daily",
+        }))
+      : [];
+
+    return fields;
   },
 };
